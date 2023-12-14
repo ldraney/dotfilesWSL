f1f7fbd8bb54e2d37c6923282869a6d3a6da46f3
ldraney <draneylucas@gmail.com>
2023-12-13 21:31:56 -0700

    toggle lines and in lua with error handling and unit tests set up successfully - 1 - unit tests working, going to make github actions

--- nvim/lua/tests/test_toggle_rel_abs_numbers.lua
+++ nvim/lua/tests/test_toggle_rel_abs_numbers.lua
@@ -1,16 +1,20 @@
 describe("toggle_line_numbers", function()
-    local toggle_line_numbers = require('toggle_rel_abs_numbers').toggle_line_numbers
+    local toggle_line_numbers
+
+    -- Mock setup before each test
+    before_each(function()
+        _G.vim = { wo = { relativenumber = false, number = false } }
+        package.loaded['toggle_rel_abs_numbers'] = nil  -- Unload the module
+        toggle_line_numbers = require('toggle_rel_abs_numbers').toggle_line_numbers
+    end)
 
     it("toggles from relative to absolute line numbers", function()
-        -- Mocking vim.wo
-        vim = { wo = { relativenumber = true, number = true } }
-
         toggle_line_numbers()
-        assert.is_false(vim.wo.relativenumber)
+        assert.is_true(vim.wo.relativenumber)
         assert.is_true(vim.wo.number)
 
         toggle_line_numbers()
-        assert.is_true(vim.wo.relativenumber)
+        assert.is_false(vim.wo.relativenumber)
         assert.is_true(vim.wo.number)
     end)
 end)
