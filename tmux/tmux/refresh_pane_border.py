7fa4a08c9a1ce7c02102a779efd33ea177bf8652
ldraney <draneylucas@gmail.com>
2024-06-20 17:01:02 -0600

    almost got tmux pane rename working on pane swithc

--- tmux/tmux/refresh_pane_border.py
+++ tmux/tmux/refresh_pane_border.py
@@ -1,24 +1,15 @@
 import libtmux
 
-def update_pane_border():
+def set_hello_world():
     # Attach to the default tmux Server
     server = libtmux.Server()
 
     # Get the first session
     session = server.sessions[0]
 
-    # Get the active window in the session
-    window = session.active_window
-
-    # Get the active pane in the window
-    pane = window.active_pane
-
-    # Get the pane title
-    pane_title = pane.current_pane_title
-
-    # Set the pane border format using the current pane title
-    session.cmd('set-option', 'pane-border-format', pane_title)
+    # Set the pane border format to "Hello World"
+    session.cmd('set-option', 'pane-border-format', 'Hi mom')
 
 if __name__ == "__main__":
-    update_pane_border()
+    set_hello_world()
 
